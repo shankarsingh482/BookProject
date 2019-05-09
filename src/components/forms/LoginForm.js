d2344ae265a462083883abbce12afc18b4e579cf
BA29575 <shankar.singh@idc.bedbath.com>
2019-05-09 15:18:09 +0530

    proxy added and api connection established

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form , Button } from 'semantic-ui-react'
+import { Form , Button, Message } from 'semantic-ui-react'
 import InlineError from '../message/InlineError'
 import PropTypes from 'prop-types'
 
@@ -11,7 +11,8 @@ class LoginForm extends React.Component {
     },
     loading:false,
     errors:{}
-  }
+  };
+
   onChange = e =>
   {
       this.setState({data:{...this.state.data,[e.target.name]:e.target.value}})
@@ -22,21 +23,24 @@ class LoginForm extends React.Component {
       this.setState({errors});
       if(Object.keys(errors).length === 0)
       {
-        this.props.submit(this.state.data);
+        //if there is any error js will find it and set the state for errors.
+        this.props.submit(this.state.data).catch(err => console.log('error is',err))
       }
 
   }
   validate = (data) =>
-{
-  if(!data.email) errors.email="email can't empty";
-  const errors ={};
-  if(!data.password) errors.password="can't be blank";
+{  const errors ={};
+  if(!data.email) errors.email="Email can't empty";
+  if(!data.password) errors.password="Can't be blank";
   return errors;
 }
   render() {
     const {data , errors}= this.state;
     return (
       <Form onSubmit={this.onSubmit}>
+      {errors.global && (<Message negative><Message.Header>Something Went Wrong</Message.Header>
+      <p>{errors.global}</p>
+      </Message>)}
       <Form.Field error = {!!errors.email}>
         <label htmlFor="email">Email</label>
          <input type="email" id="email" name="email" placeholder="example@example.com"
